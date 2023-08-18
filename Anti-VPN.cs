bdbd95f98fc88fba979324705a0a5ca539ac4d82
SploeCyber <champgamerthai@gmail.com>
2023-08-18 19:15:01 +0700

    remake

--- Anti-VPN.cs
+++ Anti-VPN.cs
@@ -1,82 +1,117 @@
 ﻿using BrokeProtocol.API;
 using BrokeProtocol.Entities;
 using BrokeProtocol.Utility.Networking;
-using Newtonsoft.Json.Linq;
-using System.IO;
 using System.Net;
-using System.Text;
 using System.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.Networking;
 
 namespace AntiVPN
 {
-    internal class Anti_VPN
+    internal class AntiVPN
     {
         [Target(GameSourceEvent.PlayerInitialize, ExecutionMode.Event)]
         public async void OnEvent(ShPlayer player)
         {
-            if (!player.isHuman) { return; }
+            if (!player.isHuman)
+                return;
+
             if (!IsLocalIpAddress(player.svPlayer.connection.IP))
             {
-                player.svPlayer.SendGameMessage("〔<color=#546eff>Anti-VPN</color>〕 |  Checking your connection.....");
-                // using large datasets from different sources with 11,500+ networks from 2,200+ privacy
-                HttpWebRequest httpWebRequest = (HttpWebRequest)WebRequest.Create($"https://whois.as207111.net/api/lookup?ip_address={player.svPlayer.connection.IP}");
-                httpWebRequest.Headers.Add("Authorization", "Bearer 120|1TxZhEle6wZ9fOngdcYzP3ac4i44fZC50TWvuZMW");
-                httpWebRequest.Accept = "application/json";
-                using (WebResponse response = httpWebRequest.GetResponse())
-                {
-                    Encoding utf = Encoding.UTF8;
-                    using (StreamReader streamReader = new StreamReader(response.GetResponseStream(), utf))
-                    {
-                        string text = streamReader.ReadToEnd();
-                        var privacy = JObject.Parse(text);
-                        var proxy = privacy["privacy"]["proxy"];
+                player.svPlayer.SendGameMessage("〔<color=#546eff>Anti-VPN</color>〕 | Checking your connection...");
 
-                        if ((bool)proxy)
+                IpInfo ipInfo = await IpInfoFetcher.CheckIfProxy(player.svPlayer.connection.IP);
+
+                if (ipInfo != null)
+                {
+                    if (ipInfo.Status != "success")
+                    {
+                        try
                         {
-                            player.svPlayer.SendGameMessage("〔<color=#546eff>Anti-VPN</color>〕 |  You will be kicked for using VPN!!!");
-                            await Task.Delay(3000);
-                            player.svPlayer.svManager.Disconnect(player.svPlayer.connection, DisconnectTypes.Kicked);
+                            if (!ipInfo.Proxy && !ipInfo.Hosting)
+                            {
+                                player.svPlayer.SendGameMessage("〔<color=#546eff>Anti-VPN</color>〕 | You will be kicked for using VPN!");
+                                await Task.Delay(3000);
+                                player.svPlayer.svManager.Disconnect(player.svPlayer.connection, DisconnectTypes.Kicked);
+                            }
+                            else
+                            {
+                                player.svPlayer.SendGameMessage("〔<color=#546eff>Anti-VPN</color>〕 | You passed the check!");
+                            }
                         }
-                        else if (!(bool)proxy)
-                        {
-                            player.svPlayer.SendGameMessage("〔<color=#546eff>Anti-VPN</color>〕 |  You passed the check!!!");
-                        }
-                        else
-                        {
-                            player.svPlayer.SendGameMessage("〔<color=#546eff>Anti-VPN</color>〕 |  Error to check your connection.");
-                        }
+                        catch { }
+                    }
+                    else if (ipInfo.Status == "fail")
+                    {
+                        player.svPlayer.SendGameMessage("〔<color=#546eff>Anti-VPN</color>〕 | You passed temporary check due to endpoint error!");
                     }
                 }
             }
             else if (IsLocalIpAddress(player.svPlayer.connection.IP) || player.svPlayer.HasPermission("avpn.bypass"))
             {
-                player.svPlayer.SendGameMessage("〔<color=#546eff>Anti-VPN</color>〕 |  Bypassed");
+                player.svPlayer.SendGameMessage("〔<color=#546eff>Anti-VPN</color>〕 | Bypassed!");
             }
         }
 
-        // https://www.tutorialsrack.com/articles/409/how-to-check-if-ipv4-ip-address-is-local-or-not-in-csharp
+        // Class to store information about the IP
+        public class IpInfo
+        {
+            public string Status;
+            public bool Proxy;
+            public bool Hosting;
+        }
+
+        // Class to fetch IP information from an API
+        public static class IpInfoFetcher
+        {
+            public static async Task<IpInfo> CheckIfProxy(string ip)
+            {
+                string apiUrl = $"https://thingproxy.freeboard.io/fetch/http://ip-api.com/json/{ip}?fields=query,status,proxy,hosting";
+                using (UnityWebRequest request = UnityWebRequest.Get(apiUrl))
+                {
+                    var tcs = new TaskCompletionSource<IpInfo>();
+
+                    request.SendWebRequest().completed += operation =>
+                    {
+                        if (request.result != UnityWebRequest.Result.Success)
+                        {
+                            Debug.LogError($"Request failed: {request.error}");
+                            tcs.SetResult(null);
+                            return;
+                        }
+
+                        string responseBody = request.downloadHandler.text;
+                        IpInfo ipInfo = JsonUtility.FromJson<IpInfo>(responseBody);
+                        tcs.SetResult(ipInfo);
+                    };
+
+                    return await tcs.Task;
+                }
+            }
+        }
+
+        // Check if an IP address is local
         public static bool IsLocalIpAddress(string host)
         {
             try
             {
-                // get host IP addresses
                 IPAddress[] hostIPs = Dns.GetHostAddresses(host);
-                // get local IP addresses
                 IPAddress[] localIPs = Dns.GetHostAddresses(Dns.GetHostName());
 
-                // test if any host IP equals to any local IP or to localhost
                 foreach (IPAddress hostIP in hostIPs)
                 {
-                    // is localhost
-                    if (IPAddress.IsLoopback(hostIP)) return true;
-                    // is local address
+                    if (IPAddress.IsLoopback(hostIP))
+                        return true;
+
                     foreach (IPAddress localIP in localIPs)
                     {
-                        if (hostIP.Equals(localIP)) return true;
+                        if (hostIP.Equals(localIP))
+                            return true;
                     }
                 }
             }
             catch { }
+
             return false;
         }
     }
